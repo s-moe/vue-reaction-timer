55f65d4a8f0e1690214590c2394d97cf2ead2818
s-moe <sarahannmoe@gmail.com>
2021-10-20 09:10:08 -0700

    made start method

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <h1>Ninja reaction timer</h1>
+  <button @click="start">play</button>
 </template>
 
 <script>
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
 };
 </script>
 
